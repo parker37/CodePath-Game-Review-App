17dd6c0ba67f452aec3f56d27c6453ef11ae78df
jreese28 <jnya.reese@gmail.com>
2022-03-29 16:00:23 -0400

    created app

---
+++ Navi-App/Navi-AppUITests/Navi_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Navi_AppUITestsLaunchTests.swift
+//  Navi-AppUITests
+//
+//  Created by Jnya Reese on 3/29/22.
+//
+
+import XCTest
+
+class Navi_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
