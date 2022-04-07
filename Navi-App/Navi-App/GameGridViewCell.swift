87ef258bdb03e703147f8edf071f31b95aca7835
parker37 <rontep@icloud.com>
2022-04-07 02:46:12 -0400

    Created Home Screen

---
+++ Navi-App/Navi-App/GameGridViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GameGridViewCell.swift
+//  Navi-App
+//
+//  Created by Ronte' Parker on 4/6/22.
+//
+
+import UIKit
+
+class GameGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coverArt: UIImageView!
+    @IBOutlet weak var gameTitle: UILabel!
+}
